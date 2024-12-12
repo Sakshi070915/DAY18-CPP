648865e24f9b53fa1c5d005d3f5dbf7c66e975f9
Sakshi Pokhriyal <sakshi.pokhriyal007@gmail.com>
2024-12-12 20:31:44 +0530

    Add files via upload

---
+++ 44.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class student{
+public:
+    int rno;
+    char name [50];
+    double fee;
+    student();
+    void display();
+};
+student::student(){
+    cout << "Enter the Roll No: ";
+    cin >> rno;
+    cout << "Enter the Name: ";
+    cin >> name;
+    cout << "Enter the Fee: ";
+    cin >> fee;
+};
+void student::display(){
+    cout << endl << rno << "\t" << name << "\t" << fee;
+}
+int main(){
+    student s;
+    s.display();
+    return 0;
+} 
